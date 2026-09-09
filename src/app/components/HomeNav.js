3dc6bba3f934ecc8b9c73962e616547727e2283b
Lewis Webster <lewis_webster@hotmail.com>
2026-09-09 19:44:50 +0100

    fix home scroll

--- src/app/components/HomeNav.js
+++ src/app/components/HomeNav.js
@@ -1,11 +1,25 @@
+"use client";
+
 import styles from "../page.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-export default function HomeNav () {
+export default function HomeNav() {
+  const router = useRouter();
 
-    return (
-        <Link href='/' className={styles.topLeft}>
-        Home     
-        </Link>
-    )
+  const handleClick = (e) => {
+    e.preventDefault();
+    // Force scroll before navigation
+    window.scrollTo(0, 0);
+    // Small delay to ensure scroll happens
+    setTimeout(() => {
+      router.push("/");
+    }, 50);
+  };
+
+  return (
+    <a href="/" className={styles.topLeft} onClick={handleClick}>
+      Home
+    </a>
+  );
 }
